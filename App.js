cdcd9aea2bdf0ea005e9b8372eef3be2a68c1b1c
Jomeesh Jose <77114748+JOMEESH-JOSE@users.noreply.github.com>
2021-12-02 01:07:44 +0530

    second

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React,{Component} from "react";
 import{
-  View,StyleSheet,ImageBackground,Image,Text
+  View,StyleSheet,ImageBackground,Image,Text, TextInput,TouchableHighlight
   
 }from 'react-native'
 export default class App extends Component{
@@ -12,6 +12,17 @@ render(){
       <Image source= {require('./images/food.jpg')} 
     style ={styles.icon}></Image>
   <Text style = {styles.text}>Come to Explore new food world</Text>
+  <TextInput style ={styles.TextInput} placeholder='username'
+  placeholderTextColor='gray' maxLength={10}>
+    
+  </TextInput>
+  <TextInput style ={styles.TextInput} placeholder='password'
+  placeholderTextColor='gray' secureTextEntry={true}>
+    
+  </TextInput>
+  <TouchableHighlight style={styles.buttonview}>
+    <Text style={styles.text1}>LOGIN</Text>
+  </TouchableHighlight>
     </ImageBackground>
     
     </View>
@@ -40,5 +51,32 @@ const styles = StyleSheet.create({
     fontSize:20,
     fontWeight:'bold',
     marginTop:20
+  },
+  TextInput:
+  {
+    width:'80%',
+    height:55,
+    borderWidth:1,
+    borderColor:'green',
+    marginTop:50,
+    paddingLeft:20
+  },
+  buttonview:{
+    width:'60%',
+    height:55,
+    backgroundColor:'green',
+    marginTop:30,
+    borderRadius:8,
+    alignItems:'center',
+    justifyContent:'center'
+    
+  },
+  text1:{
+    fontSize:20,
+    fontWeight:'bold',
+    marginTop:10,
+    color:'white'
+   
+
   }
 }) 
