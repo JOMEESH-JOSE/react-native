a85769451031519f828c84675ecb2265473b0dba
Jomeesh Jose <77114748+JOMEESH-JOSE@users.noreply.github.com>
2021-12-02 22:07:33 +0530

    third

--- Flexbox.js
+++ Flexbox.js
@@ -1,15 +1,47 @@
 import React,{Component} from "react";
 import{
-    View,StyleSheet,Text
+    View,StyleSheet,Text,TouchableHighlight
 } from 'react-native'
+class FlexContent extends  Component{
+    render(){
+        return(
+            <View>
+                <Text style={styles.text1}>hi {this.props.name}</Text>
+            </View>
+        )
+    }
+}
 export default class Flexbox extends Component
 {
+    constructor(){
+        super();
+        this.state = {
+            main_text : "jomeesh",
+            subtext : "coming"
+        }
+    }
+    update_text(){
+        this.setState({
+            main_text : 'JMJ',
+            subtext:'come'
+            
+        })
+    }
     render(){
         return(
            <View style = {styles.container}>
-            <Text style={styles.text}>Hello</Text>
-            <Text style={styles.text}> Foodies !</Text>
+            <Text style={styles.text}>{this.state.main_text}</Text>
+            <Text style={styles.text}>{this.state.subtext}</Text>
+            <TouchableHighlight style={styles.Button}
+            underlayColor='red'
+            onPress={()=>this.update_text()} >
+                <Text style={styles.Button_text}>UPDATE</Text>
+            </TouchableHighlight>
+            <FlexContent name ='jomeesh' > 
+
+            </FlexContent>
            </View>
+           
         )
     }
 
@@ -17,13 +49,34 @@ export default class Flexbox extends Component
 const styles = StyleSheet.create({
     container : {
         flex :1,
-        backgroundColor:'red',
+        backgroundColor:'gray',
         alignItems:'center',
         justifyContent:'center',
-        flexDirection:'row'
+        // flexDirection:'row'
     },
     text:{
-        color:'white',
+        color:'black',
         fontSize:24  
-    }
+    },
+    Button:{
+        height:50,
+        width:'60%',
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'green',
+        borderRadius:10,
+        marginTop:20
+    },
+    Button_text:{
+        fontSize:20,
+        fontWeight:'bold',
+        alignItems:'center'
+
+
+},
+text1:{
+    color:'blue',
+    fontSize:24,
+    marginTop:20 
+}
 })
